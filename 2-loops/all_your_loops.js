115495e3456641a677f094b5121ca586a626d8ca
Barrett Amsrud <barrettamsrud@Barretts-MacBook-Pro.local>
2016-09-26 14:50:10 -0500

    Completed module 2

--- 2-loops/all_your_loops.js
+++ 2-loops/all_your_loops.js
@@ -3,8 +3,13 @@
     value of checkz by 1.
 */
 var checkz = 0;
-
+for (var i = 0; i < 5; i ++) {
+  checkz ++;
+}
 /*
 2 - Write a while loop that runs 3 times. Each iteration will decrease
     the value of checkz by 2
 */
+while (checkz > 0) {
+  checkz -= 2;
+}
