5f8915a899491c1f24563e35408e03cb434fa59b
Barrett Amsrud <barrettamsrud@Barretts-MacBook-Pro.local>
2016-09-26 15:23:45 -0500

    Completed module 6

--- 6-objects/literal.js
+++ 6-objects/literal.js
@@ -5,13 +5,23 @@
     - zip
 Assign the values based on Prime's address.
 */
-
+var prime = {
+  city: 'Bloomington',
+  state: 'MN',
+  zip: 55431
+};
 /*
 2 - Using dot notation, create a new property for prime. This property will
     be called address and will  take the value of Prime's street address.
 */
-
+prime.address = '9401 James Ave S, Suite 152';
+console.log(prime);
 /*
 3 - Using dot notation, create a new method for prime. This method will
     console log out the complete address of prime. Call the new property/method print.
 */
+prime.print = function() {
+  console.log(prime.address + '\n' + prime.city + ', ' + prime.state + ' ' + prime.zip);
+}
+
+prime.print();
